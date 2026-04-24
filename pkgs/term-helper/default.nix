e32dbbdbcf72d91ef18edf019e2de09bc8b389fa
Anon4You <katilchora666@gmail.com>
2026-04-25 00:42:21 +0530

    test install pkgs

--- pkgs/term-helper/default.nix
+++ pkgs/term-helper/default.nix
@@ -1,22 +1,21 @@
-with import <nixpkgs> {};
-callPackage ({stdenvNoCC, fetchurl, curl, pv, makeWrapper, lib}:
-  stdenvNoCC.mkDerivation{
-    pname = "term-helper";
-    version = "1.1.1";
+{ stdenvNoCC, fetchurl, pv, curl, makeWrapper, lib }:
 
-    src = fetchurl {
-      url = "https://github.com/Anon4You/term-helper/raw/main/term-helper.sh";
-      hash = "sha256-O26VIxjxQay7US5artA5v/D+5Qs2wIRj0UcmU8wrZFU=";
-    };
-
-    nativeBuildInputs = [makeWrapper];
-    dontUnpack = true;
-
-    installPhase = ''
+let
+  pname = "term-helper";
+in
+stdenvNoCC.mkDerivation {
+  inherit pname;
+  version = "1.1.1";
+  src = fetchurl {
+    url = "https://github.com/Anon4You/term-helper/raw/main/term-helper.sh";
+    hash = "sha256-O26VIxjxQay7US5artA5v/D+5Qs2wIRj0UcmU8wrZFU=";
+  };
+  nativeBuildInputs = [ makeWrapper ];
+  dontUnpack = true;
+  installPhase = ''
     mkdir -p $out/bin
-    cp $src $out/bin/$pname
-    chmod +x $out/bin/$pname
-    wrapProgram $out/bin/$pname --prefix PATH : ${lib.makeBinPath [pv curl]}
-    '';
-  }
-){}
+    cp $src $out/bin/${pname}
+    chmod +x $out/bin/${pname}
+    wrapProgram $out/bin/${pname} --prefix PATH : ${lib.makeBinPath [pv curl]}
+  '';
+}
